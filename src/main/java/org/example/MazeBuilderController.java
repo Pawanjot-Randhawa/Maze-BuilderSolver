3504a99a254c089ed8b02e32cec47a677d21b718
Pawanjot-Randhawa <pawanjot2004@gmail.com>
2025-03-19 11:10:48 -0400

    Updated rectangle clicking logic to prevent bugs.

--- src/main/java/org/example/MazeBuilderController.java
+++ src/main/java/org/example/MazeBuilderController.java
@@ -21,6 +21,7 @@
        private int startPoint;
        private int endPoint;
        private ToggleGroup tools;
+       private Rectangle lastStart;
 
        //setting colors
        Color start = Color.LIGHTGREEN;
@@ -131,13 +132,9 @@
                    }else if (mazeArray[row][col]==8) {
                        cell.setFill(start);
                        this.startPoint = 1;
-                       startNum.setTextFill(Color.GREEN);
-                       startNum.setText(String.valueOf(startPoint));
                    }else if (mazeArray[row][col]==3) {
                        cell.setFill(end);
                        this.endPoint += 1;
-                       endNum.setTextFill(Color.GREEN);
-                       endNum.setText(String.valueOf(endPoint));
                    }
                    cell.setStroke(Color.BLACK);  // Add a border
                    // Dynamically bind the size of the rectangle
@@ -147,6 +144,7 @@
                    cell.setOnMouseClicked(event -> handleCellClick(event, cell));
                    // Add the rectangle to the GridPane at the correct position
                    grid.add(cell, col, row);
+                   update();
                }
            }
        }
@@ -159,6 +157,23 @@
            endPoint = 0;
            endNum.setTextFill(Color.RED);
            endNum.setText(String.valueOf(endPoint));
+       }
+
+       public void update(){
+           if(startPoint==1){
+               startNum.setTextFill(Color.GREEN);
+               startNum.setText(String.valueOf(startPoint));
+           } else if (startPoint==0) {
+               startNum.setTextFill(Color.RED);
+               startNum.setText(String.valueOf(startPoint));
+           }
+           if(endPoint>0){
+               endNum.setTextFill(Color.GREEN);
+               endNum.setText(String.valueOf(endPoint));
+           } else if (endPoint==0) {
+               endNum.setTextFill(Color.RED);
+               endNum.setText(String.valueOf(endPoint));
+           }
        }
 
        private Maze getMazeArray(){
@@ -186,49 +201,56 @@
 
            Toggle selectedToggle = tools.getSelectedToggle(); // Get the selected toggle in the group
 
-           if (selectedToggle == startToggle) { //start toggle
-               if (startPoint<1 && !cell.getFill().equals(start)) { // no start points means we can set one
+           if (selectedToggle == startToggle) { //start toggle; no matter where we click we want this to make a start, we can also only have one start
+               if (startPoint<1 && cell.getFill().equals(end)) { // no start points and we click on an end, make it a start point
                    cell.setFill(start);
                    startPoint = 1;
-                   startNum.setTextFill(Color.GREEN);
-                   startNum.setText(String.valueOf(startPoint));
-               } else {
-                   if(cell.getFill().equals(start)){ //delete start
+                   lastStart = cell;
+                   endPoint -= 1;
+               }else if(startPoint<1){ // no start point and we click on path or wall, make it a start
+                   cell.setFill(start);
+                   startPoint = 1;
+                   lastStart = cell;
+               }else { // branch if a start point already exists
+                   if(cell.getFill().equals(start)){ //delete start if we click on start
                        cell.setFill(path);
                        startPoint = 0;
-                       startNum.setTextFill(Color.RED);
-                       startNum.setText(String.valueOf(startPoint));
-
-                   }else{ //default behaviour if cell is not a start
-                       cell.setFill(path);
+                   }else if(cell.getFill().equals(end)){//if start toggle is on and you hit an end point, replace the end point with start and remove last start
+                       cell.setFill(start);
+                       lastStart.setFill(path);
+                       lastStart = cell;
+                       endPoint -= 1;
+                   }else{ //if cell is not start or end, switch the last start to path and move new start here
+                       cell.setFill(start);
+                       lastStart.setFill(path);
+                       lastStart = cell;
                    }
                }
-           } else if (selectedToggle == endToggle) { // end toggle
-               if (!cell.getFill().equals(end)) { //no limit on end points so we cant just start making them
+           } else if (selectedToggle == endToggle) { // end toggle; we want to make end points where we click, we can have as many end points as we want
+               if(cell.getFill().equals(end)){ // delete end point if we click on end point
+                   cell.setFill(path);
+                   endPoint -= 1;
+               }else if(cell.getFill().equals(start)){//if end toggle is on and you hit a start, replace the start with an end point
+                   cell.setFill(end);
+                   startPoint = 0;
+                   endPoint += 1;
+               }else{ //default behaviour if cell is not a end point
                    cell.setFill(end);
                    endPoint += 1;
-                   endNum.setTextFill(Color.GREEN);
-                   endNum.setText(String.valueOf(endPoint));
-               } else {
-                   if(cell.getFill().equals(end)){ // delete end point
-                       cell.setFill(path);
-                       endPoint -= 1;
-                       endNum.setTextFill(Color.RED);
-                       endNum.setText(String.valueOf(endPoint));
-                   }else{ //default behaviour if cell is not a end point
-                       cell.setFill(path);
-                   }
                }
            } else {     //default behaviour
-               if (cell.getFill().equals(path)) {
+               if (cell.getFill().equals(path)) { // if we click on path, set it to wall
                    cell.setFill(wall);
-               } else {
+               }else if(cell.getFill().equals(start)){ // if we click on start, set it to path
+                   cell.setFill(path);
+                   startPoint = 0;
+               }else if(cell.getFill().equals(end)){ //if we click on end, set it to path
+                   cell.setFill(path);
+                   endPoint -= 1;
+               }else { // if we click on a wall, we set it to path
                    cell.setFill(path);
                }
            }
-
-
-
+           update();
        }
-
    }
