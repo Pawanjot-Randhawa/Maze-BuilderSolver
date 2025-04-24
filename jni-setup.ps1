6cd5882fc2b0760a156427f0ec03d1fe355543f2
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-04-24 12:43:29 -0400

    updated structure to have a build dir to hold object files

--- jni-setup.ps1
+++ jni-setup.ps1
@@ -2,6 +2,7 @@ Write-Host "Java path: $env:JAVA_HOME"
 $nativeDIR = "lib/src/"
 $headersDIR = "lib/headers"
 $srcDIR="src/main/java/org/example/"
+$buildDIR="build"
 
 Write-Host "Generating headers into lib/headers"
 javac -h $headersDIR/ $srcDIR/native/**.java $srcDIR/Maze.java
@@ -13,15 +14,15 @@ $JAVA_INCLUDE_WIN32_PATH = "$env:JAVA_HOME\include\win32"
 # Compile the C++ source code
 Write-Host "Compiling C++ Code" 
 
-g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\org_example_Native_MazeSolver.cpp -o Solver.o
-g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\Logger.cpp -o Log.o
-g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\Matrix.cpp -o Matrix.o
-g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\AStar.cpp -o AStar.o
-g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\Dijkstra.cpp -o Dijkstra.o
-g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\BlockNdPath.cpp -o BlockNdPath.o
+g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\org_example_Native_MazeSolver.cpp -o $buildDIR\Solver.o
+g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\Logger.cpp -o $buildDIR\Log.o
+g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\Matrix.cpp -o $buildDIR\Matrix.o
+g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\AStar.cpp -o $buildDIR\AStar.o
+g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\Dijkstra.cpp -o $buildDIR\Dijkstra.o
+g++ -c -I"$JAVA_INCLUDE_PATH" -I"$JAVA_INCLUDE_WIN32_PATH" $nativeDIR\BlockNdPath.cpp -o $buildDIR\BlockNdPath.o
 
 
 
 # Linking C++ Code
 Write-Host "Linking C++ Code"
-g++ --% -shared -o native.dll Solver.o Log.o Matrix.o AStar.o Dijkstra.o BlockNdPath.o -Wl,--add-stdcall-alias
+g++ --% -shared -o native.dll build\*.o -Wl,--add-stdcall-alias
