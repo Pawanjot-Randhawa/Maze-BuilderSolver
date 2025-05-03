f25a66b8734554ed06bd493a938eae8f4879f5c3
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-05-03 18:05:04 -0400

    Added 1st unit test

--- src/test/java/org/example/TestNativeMazeSolver.java
+++ src/test/java/org/example/TestNativeMazeSolver.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import org.example.Native.MazeSolver;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TestNativeMazeSolver {
-    
+    @Test
+    public void TestLibraryLoaded() {
+        assertEquals("returned from cpp code", MazeSolver.returnHello());
+    }    
 }
