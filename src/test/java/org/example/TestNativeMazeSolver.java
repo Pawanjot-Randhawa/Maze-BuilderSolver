fb4384e2d5367fa9daae4ff17b4f1f8c175bcde8
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-05-03 19:13:38 -0400

    Added IDAStar tests and created complex maze

--- src/test/java/org/example/TestNativeMazeSolver.java
+++ src/test/java/org/example/TestNativeMazeSolver.java
@@ -10,10 +10,10 @@ import org.junit.Test;
 
 public class TestNativeMazeSolver {
 
-    private final int width = 5;
-    private final int height = 5;
+    private final int SIMPLE_WIDTH = 5;
+    private final int SIMPLE_HEIGHT = 5;
 
-    private final int[][] SimpleMazeWith1Goal = {
+    private final int[][] SimpleMazeWithSingleGoal = {
         { Path.WALL.get(), Path.PATH.get(), Path.PATH.get(), Path.WALL.get(), Path.PATH.get() },
         { Path.PATH.get(), Path.WALL.get(), Path.WALL.get(), Path.PATH.get(), Path.PATH.get() },
         { Path.WALL.get(), Path.PATH.get(), Path.WALL.get(), Path.PATH.get(), Path.GOAL.get() },
@@ -21,8 +21,9 @@ public class TestNativeMazeSolver {
         { Path.WALL.get(), Path.WALL.get(), Path.PATH.get(), Path.PATH.get(), Path.START.get() }, 
     };
 
-    @SuppressWarnings({ "ignore", "unused" })
-    private final int[][] ComplexMazeWith2Goals = {
+    private final int COMPLEX_WIDTH = 6;
+    private final int COMPLEX_HEIGHT = 6;
+    private final int[][] ComplexMazeWithMultipleGoals = {
         { Path.WALL.get(), Path.PATH.get(), Path.PATH.get(), Path.WALL.get(), Path.PATH.get() },
         { Path.PATH.get(), Path.WALL.get(), Path.WALL.get(), Path.PATH.get(), Path.PATH.get() },
         { Path.WALL.get(), Path.PATH.get(), Path.WALL.get(), Path.PATH.get(), Path.GOAL.get() },
@@ -30,8 +31,20 @@ public class TestNativeMazeSolver {
         { Path.WALL.get(), Path.WALL.get(), Path.PATH.get(), Path.PATH.get(), Path.START.get() }, 
     };
 
-    private final Maze maze = new Maze(this.SimpleMazeWith1Goal, this.width, this.height);
+    private final Maze simpleMaze = new Maze(
+        this.SimpleMazeWithSingleGoal, 
+        this.SIMPLE_WIDTH, 
+        this.SIMPLE_HEIGHT
+    );
+    
+    private final Maze complexMaze = new Maze(
+        this.ComplexMazeWithMultipleGoals, 
+        this.COMPLEX_WIDTH, 
+        this.COMPLEX_HEIGHT
+    );
 
+
+    
     @Test
     public void TestLibraryLoaded() {
         assertEquals("returned from cpp code", MazeSolver.returnHello());
@@ -47,7 +60,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_AStar_Simple_Maze_Correct_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                             .SolveWith(SolveStrategy.ASTAR);
         
         final int[][] expectedSteps = {
@@ -72,7 +85,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_AStar_Simple_Maze_Wrong_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                             .SolveWith(SolveStrategy.ASTAR);
         
         final int[][] expectedSteps = {
@@ -100,7 +113,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_Dijkstra_Simple_Maze_Correct_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                                     .SolveWith(SolveStrategy.DIJKSTRA);
 
         final int[][] expectedSteps = {
@@ -125,7 +138,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_Dijkstra_Simple_Maze_Wrong_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                                     .SolveWith(SolveStrategy.DIJKSTRA);
 
         final int[][] expectedSteps = {
@@ -153,7 +166,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_BlockNdPath_Simple_Maze_Correct_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                                     .SolveWith(SolveStrategy.BLOCKNDPATH);
 
         final int[][] expectedSteps = {
@@ -179,7 +192,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_BlockNdPath_Simple_Maze_Wrong_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                                     .SolveWith(SolveStrategy.BLOCKNDPATH);
 
         final int[][] expectedSteps = {
@@ -213,7 +226,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_BFS_Simple_Maze_Correct_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                                     .SolveWith(SolveStrategy.BFS);
 
         final int[][] expectedSteps = {
@@ -240,7 +253,7 @@ public class TestNativeMazeSolver {
 
     @Test
     public void Test_BFS_Simple_Maze_Wrong_Path() {
-        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.maze)
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
                                                     .SolveWith(SolveStrategy.BFS);
 
         final int[][] expectedSteps = {
@@ -270,6 +283,65 @@ public class TestNativeMazeSolver {
             });
     }
 
+
+
+
+    @Test
+    public void Test_IDAStar_Simple_Maze_Correct_Path() {
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
+                                                    .SolveWith(SolveStrategy.IDASTAR);
+
+        final int[][] expectedSteps = {
+            {4,4},
+            {4,3},
+            {4,2},
+            {3,1},
+            {2,1},
+            {1,0},
+            {0,1},
+            {0,2},
+            {1,3},
+            {0,4},
+            {2,4},
+        };
+
+        IntStream.range(0, receivedSteps.size())
+            .forEach(i -> {
+                final boolean isMatch = Arrays.equals(receivedSteps.get(i), expectedSteps[i]);
+                assertEquals(true, isMatch);
+            });
+    }
+
+    @Test
+    public void Test_IDAStar_Simple_Maze_Wrong_Path() {
+        final List<int[]> receivedSteps = SolverAPI.GetInstance(this.simpleMaze)
+                                                    .SolveWith(SolveStrategy.IDASTAR);
+
+        final int[][] expectedSteps = {
+            {2,4},
+            {0,4},
+            {1,3},
+            {0,2},
+            {0,1},
+            {4,4},
+            {2,1},
+            {3,1},
+            {4,2},
+            {4,3},
+            {4,4},
+        };
+
+        IntStream.range(0, receivedSteps.size())
+            .forEach(i -> {
+                final boolean isMatch = Arrays.equals(receivedSteps.get(i), expectedSteps[i]);
+                if(isMatch) {
+                    System.out.println("Maze is: " + Arrays.toString(receivedSteps.get(i)));
+                    System.out.println("Expected Maze is: " + Arrays.toString(expectedSteps[i]));
+
+                }
+                assertEquals(false, isMatch);
+            });
+    }
 
 
 
